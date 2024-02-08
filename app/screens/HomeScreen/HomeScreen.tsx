f79bc4182f8a9c0fa464470d495b1cc0875bbcf4
wyhong3103 <wyhong3103@gmail.com>
2024-02-08 13:38:13 +0800

    Add margin top

--- app/screens/HomeScreen/HomeScreen.tsx
+++ app/screens/HomeScreen/HomeScreen.tsx
@@ -17,17 +17,17 @@ export const HomeScreen: FC<HomeScreenProps> = ({navigation}) => {
             <View style={$btnContainer}>
                 <Button
                     text='NEW GAME'
-                    onPress={() => console.log("new")}
-                    style={[{backgroundColor: "#8873FF"}, { borderRadius: 10 }]}
-                    pressedStyle={[{backgroundColor: "#A493FF"}, { borderRadius: 10 }]}
-                    textStyle={[{ fontSize: 20 }, { color: "white" }]}
+                    onPress={() => navigation.navigate('NewGame')}
+                    style={$primaryBtn}
+                    pressedStyle={$primaryBtnPressed}
+                    textStyle={$btnText}
                 />
                 <Button
                     text='IMPORT GAME'
                     onPress={() => console.log("import")}
-                    style={[{backgroundColor: "#9F9F9F"}, { borderRadius: 10 }]}
-                    pressedStyle={[{backgroundColor: "#C2C2C2"}, { borderRadius: 10 }]}
-                    textStyle={[{ fontSize: 20 }, { color: "white" }]}
+                    style={$secondaryBtn}
+                    pressedStyle={$secondaryBtnPressed}
+                    textStyle={$btnText}
                 />
             </View>
         </View>
@@ -42,10 +42,36 @@ const $container: ViewStyle = {
 const $titleContainer: ViewStyle = {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    marginTop: 100
 }
 
 const $btnContainer: ViewStyle = {
     padding: 20,
     gap: 10
+}
+
+const $primaryBtn = {
+    backgroundColor: "#8873FF",  
+    borderRadius: 10 
+}
+
+const $primaryBtnPressed = {
+    backgroundColor: "#A493FF",  
+    borderRadius: 10 
+}
+
+const $secondaryBtn = {
+    backgroundColor: "#9F9F9F",  
+    borderRadius: 10 
+}
+
+const $secondaryBtnPressed = {
+    backgroundColor: "#C2C2C2",  
+    borderRadius: 10 
+}
+
+const $btnText = {
+    fontSize: 20,
+    color: "white"
 }
