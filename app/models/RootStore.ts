df4ee2a15fd00527b882795f5d1c4e53fbb9d7b3
wyhong3103 <wyhong3103@gmail.com>
2024-02-09 17:13:07 +0800

    Add sudoku puzzle state

--- app/models/RootStore.ts
+++ app/models/RootStore.ts
@@ -1,9 +1,18 @@
 import { Instance, SnapshotOut, types } from "mobx-state-tree"
+import { SudokuStoreModel } from "./SudokuStore"
 
 /**
  * A RootStore model.
  */
 export const RootStoreModel = types.model("RootStore").props({
+    sudokuStore: types.optional(SudokuStoreModel, {
+        puzzle: [],
+        mask: [],
+        selected: {
+            row: undefined,
+            col: undefined
+        }
+    })
 })
 
 /**
---
+++ app/models/SudokuStore.ts
@@ -0,0 +1,44 @@
+import { Instance, SnapshotOut, types, cast } from "mobx-state-tree"
+
+const MatrixType = types.array(types.array(types.number));
+
+const SelectedModel = types
+    .model("Selected")
+    .props({
+        row: types.maybe(types.number),
+        col: types.maybe(types.number)
+    })
+    .actions((store) => ({
+        setRow(row?: number){
+            store.row = row
+        },
+        setCol(col?: number){
+            store.col = col
+        }
+    }))
+
+export const SudokuStoreModel = types
+    .model("SudokuStore")
+    .props({
+        puzzle: MatrixType,
+        mask: MatrixType,
+        selected: SelectedModel
+    })
+    .actions((store) => ({
+        setItem(row: number, col: number, value: number) {
+            store.puzzle[row][col] = value
+        },
+        setPuzzle(puzzle: number[][]){
+            store.puzzle = cast(puzzle)
+        },
+        setMask(mask: number[][]){
+            store.mask = cast(mask)
+        },
+        setSelected(row?:number, col?:number){
+            store.selected.setRow(row);
+            store.selected.setCol(col);
+        }
+    }))
+
+export interface SudokuStore extends Instance<typeof SudokuStoreModel> {}
+export interface SudokuStoreSnapshot extends SnapshotOut<typeof SudokuStoreModel> {}
